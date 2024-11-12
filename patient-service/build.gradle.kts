cceae8196890aefbe16fbb5fd08f13224262732c
Jamaal Long <coryell1287@gmail.com>
2024-11-12 15:31:44 -0600

    Create Patient Controller #3

---
+++ patient-service/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.pam"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.retry:spring-retry:2.0.10")
+
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
