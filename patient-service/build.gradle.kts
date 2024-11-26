49ee8c049ced4f6231da091d51a7b183bbf86ca2
Jamaal Long <coryell1287@gmail.com>
2024-11-25 19:44:58 -0600

    issue-24 • Add checkstyle configuration to gradle • Add .dockerignore file • Update commit-stage to handle artifacts from checkstyle

--- patient-service/build.gradle.kts
+++ patient-service/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.5"
-	id("io.spring.dependency-management") version "1.1.6"
-	id("jacoco")
-	id("com.github.spotbugs") version "6.0.18"
+    java
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("jacoco")
+    id("com.github.spotbugs") version "6.0.18"
+    id("checkstyle")
 }
 
 group = "com.pam"
@@ -11,123 +12,136 @@ version = "0.0.1-SNAPSHOT"
 
 
 java {
-	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(17))
-	}
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 jacoco {
-	toolVersion = "0.8.11"
-	reportsDirectory.set(layout.buildDirectory.dir("reports/jacoco"))
+    toolVersion = "0.8.11"
+    reportsDirectory.set(layout.buildDirectory.dir("reports/jacoco"))
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 val springCloudVersion = "2023.0.3"
 
 dependencies {
-	constraints {
-		implementation("com.fasterxml.jackson:jackson-bom:2.16.1")
-		implementation("org.apache.commons:commons-lang3:3.14.0")
-		implementation("org.apache.commons:commons-text:1.11.0")
-	}
+    constraints {
+        implementation("com.fasterxml.jackson:jackson-bom:2.16.1")
+        implementation("org.apache.commons:commons-lang3:3.14.0")
+        implementation("org.apache.commons:commons-text:1.11.0")
+    }
 
-	// Spring Boot dependencies
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.cloud:spring-cloud-starter-config")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.retry:spring-retry:2.0.10")
+    // Spring Boot dependencies
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.retry:spring-retry:2.0.10")
 
-	// Lombok configuration
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testCompileOnly("org.projectlombok:lombok")
-	testAnnotationProcessor("org.projectlombok:lombok")
+    // Lombok configuration
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
 
-	// Development and testing dependencies
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Development and testing dependencies
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
-	}
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.test {
-	useJUnitPlatform {
-		excludeTags("integration")
-	}
-	finalizedBy(tasks.jacocoTestReport)
+    useJUnitPlatform {
+        excludeTags("integration")
+    }
+    finalizedBy(tasks.jacocoTestReport)
 }
 
+
 val integrationTest = tasks.register<Test>("integrationTest") {
-	description = "Runs integration tests."
-	group = "verification"
+    description = "Runs integration tests."
+    group = "verification"
 
-	useJUnitPlatform {
-		includeTags("integration")
-	}
+    useJUnitPlatform {
+        includeTags("integration")
+    }
 
-	mustRunAfter(tasks.test)
+    mustRunAfter(tasks.test)
 }
 
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
+    dependsOn(tasks.test)
 
-	classDirectories.setFrom(
-			files(classDirectories.files.map {
-				fileTree(it) {
-					exclude("**/model/**", "**/config/**")
-				}
-			})
-	)
+    classDirectories.setFrom(
+        files(classDirectories.files.map {
+            fileTree(it) {
+                exclude("**/model/**", "**/config/**")
+            }
+        })
+    )
 
-	reports {
-		xml.required.set(true)
-		csv.required.set(false)
-		html.required.set(true)
-		html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco/html"))
-	}
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco/html"))
+    }
 }
 
 tasks.jacocoTestCoverageVerification {
-	dependsOn(tasks.jacocoTestReport)
+    dependsOn(tasks.jacocoTestReport)
 
-	violationRules {
-		rule {
-			limit {
-				counter = "LINE"
-				value = "COVEREDRATIO"
-				minimum = "0.80".toBigDecimal()
-			}
-			limit {
-				counter = "BRANCH"
-				value = "COVEREDRATIO"
-				minimum = "0.80".toBigDecimal()
-			}
-			limit {
-				counter = "INSTRUCTION"
-				value = "COVEREDRATIO"
-				minimum = "0.80".toBigDecimal()
-			}
-		}
-	}
+    violationRules {
+        rule {
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.80".toBigDecimal()
+            }
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.80".toBigDecimal()
+            }
+            limit {
+                counter = "INSTRUCTION"
+                value = "COVEREDRATIO"
+                minimum = "0.80".toBigDecimal()
+            }
+        }
+    }
+}
+
+val checkstyle = tasks.withType<Checkstyle>().configureEach {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+
+    description = "Runs checkstyle to enforce code style and formatting standards."
+
+    reports {
+        xml.required = false
+        html.required = true
+        html.stylesheet = resources.text.fromFile("config/xsl/checkstyle-custom.xsl")
+    }
 }
 
 tasks.check {
-	dependsOn(
-			integrationTest,
-			tasks.jacocoTestCoverageVerification,
-	)
+    dependsOn(
+        integrationTest,
+        tasks.jacocoTestCoverageVerification,
+    )
 }
--- patient-service/src/test/java/com/pam/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/pam/patientservice/PatientServiceApplicationTests.java
@@ -3,20 +3,14 @@ package com.pam.patientservice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest
 class PatientServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	@DisplayName("Verify main method doesn't throw any exceptions")
-	void main_shouldStartApplication() {
-		PatientServiceApplication.main(new String[]{});
-	}
+    @Test
+    @DisplayName("Verify main method doesn't throw any exceptions")
+    void mainShouldStartApplication() {
+        PatientServiceApplication.main(new String[]{});
+    }
 }
