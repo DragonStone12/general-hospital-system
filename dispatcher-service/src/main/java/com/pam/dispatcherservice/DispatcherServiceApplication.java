cc94303421f959a870a2edab0454a1d2534e8d9e
Jamaal Long <coryell1287@gmail.com>
2025-01-31 11:01:01 -0600

    issue-48 Add Angular
    * Fix conditional to include provider service

--- dispatcher-service/src/main/java/com/pam/dispatcherservice/DispatcherServiceApplication.java
+++ dispatcher-service/src/main/java/com/pam/dispatcherservice/DispatcherServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @SpringBootApplication
 @RefreshScope
 public class DispatcherServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DispatcherServiceApplication.class, args);
     }
